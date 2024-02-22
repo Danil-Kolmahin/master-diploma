1f1c1f396b577326cdfbd330975bcbed60dc70fe
Danil-Kolmahin <danil.kolmahin@gmail.com>
2024-02-22 10:36:26 +0000

    Test front and back communication

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,12 +1,18 @@
 import styled from '@emotion/styled';
 
 import NxWelcome from './nx-welcome';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+  useEffect(() => {
+    axios('/version').then(r => console.log(r));
+  }, []);
+
   return (
     <StyledApp>
       <NxWelcome title="frontend" />
