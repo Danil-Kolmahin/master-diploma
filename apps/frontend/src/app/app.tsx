864505a12e92a7281a618132ca6f778c6ee50d7a
Danil-Kolmahin <danil.kolmahin@gmail.com>
2024-03-24 16:51:12 +0200

    Highcharts following test

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -9,19 +9,65 @@ require('highcharts/modules/venn.js')(Highcharts)
 const options = {
   title: { text: '' },
   credits: { enabled: false },
+  tooltip: {
+    headerFormat:
+      '<span style="color:{point.color}">\u2022</span><span style="font-size: 14px"> {point.point.name}</span><br/>',
+    pointFormat: '{point.longDescription}'
+  },
   series: [{
     type: 'venn',
     data: [{
-      sets: ['A'],
-      value: 4,
-      name: 'Euler diagrams',
+      sets: ['local'],
+      value: 100,
+      longDescription: 'PORT=8090\nPV_KEY=test'
     }, {
-      sets: ['B'],
-      value: 1,
-      name: 'Venn diagrams',
+      sets: ['qa'],
+      value: 100,
     }, {
-      sets: ['A', 'B'],
-      value: 1
+      sets: ['prod'],
+      value: 100,
+    }, {
+      sets: ['local', 'qa'],
+      value: 50,
+    }, {
+      sets: ['qa', 'prod'],
+      value: 50,
+    }, {
+      sets: ['prod', 'local'],
+      value: 50,
+    }, {
+      sets: ['local', 'qa', 'prod'],
+      value: 50,
+    }, {
+      sets: ['dev1'],
+      value: 10,
+    }, {
+      sets: ['dev2'],
+      value: 10,
+    }, {
+      sets: ['dev3'],
+      value: 10,
+    }, {
+      sets: ['local', 'dev1'],
+      value: 10,
+    }, {
+      sets: ['local', 'dev2'],
+      value: 10,
+    }, {
+      sets: ['local', 'dev3'],
+      value: 10,
+    }, {
+      sets: ['dev1', 'dev2'],
+      value: 5,
+    }, {
+      sets: ['dev2', 'dev3'],
+      value: 5,
+    }, {
+      sets: ['dev3', 'dev1'],
+      value: 5,
+    }, {
+      sets: ['dev1', 'dev2', 'dev3'],
+      value: 5,
     }]
   }],
 }
