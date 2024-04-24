ce4f997205907a966290a611bc2be5a266cfa04d
Danil-Kolmahin <danil.kolmahin@gmail.com>
2024-04-24 16:46:55 +0300

    Invites WIP +2

--- apps/backend/src/app/controllers/namespaces.controller.ts
+++ apps/backend/src/app/controllers/namespaces.controller.ts
@@ -1,16 +1,43 @@
-import { AuthGuard, NamespacesService } from '@master-diploma/library';
+import {
+  AuthGuard,
+  CasbinService,
+  NamespacesService,
+} from '@master-diploma/library';
 import { Body, Controller, Post, UseGuards, Req, Get } from '@nestjs/common';
 import { Request } from 'express';
 import { NamespaceDto } from '../dto/namespace.dto';
 
 @Controller('namespaces')
 export class NamespacesController {
-  constructor(private readonly namespacesService: NamespacesService) {}
+  constructor(
+    private readonly namespacesService: NamespacesService,
+    private readonly casbinService: CasbinService
+  ) {}
 
   @Get('all')
   @UseGuards(AuthGuard)
-  findByProjectId(@Req() req: Request) {
-    return this.namespacesService.findByProjectId((req as any).user.projectId);
+  async findByProjectId(@Req() req: Request) {
+    const namespaces = await this.namespacesService.findByProjectId(
+      (req as any).user.projectId
+    );
+
+    const rights = await Promise.all(
+      namespaces.map((n) =>
+        this.casbinService.enforce(
+          (req as any).user.sub,
+          n.id,
+          'read',
+          (req as any).user.projectId
+        )
+      )
+    );
+
+    const result = [];
+
+    for (let i = 0; i < namespaces.length; i++)
+      if (rights[i]) result.push(namespaces[i]);
+
+    return result;
   }
 
   @Post()
