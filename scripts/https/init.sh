018787beab68d7a288f4afb34fe68e5f2cd49306
Danil Kolmahin <danil.kolmahin@gmail.com>
2024-05-03 15:56:06 +0300

    HTTPS

---
+++ scripts/https/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
+kubectl apply -f scripts/https/staging-issuer.yaml
+kubectl apply -f scripts/https/ingress.yaml
+
+# kubectl delete secrets api
+# kubectl apply -f scripts/https/production-issuer.yaml
+# kubectl apply -f scripts/https/ingress.yaml
+
+# https://www.datascienceengineer.com/blog/post-traefik-automatic-ssl-certificates#change-issuer-to-lets-encrypt-production
+
+# kubectl port-forward -n kube-system deployment/traefik 9000:9000
+# http://localhost:9000/dashboard/
